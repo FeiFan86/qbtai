b7156e1b7df0916403ec70bbf61236404c9268db
FeiFan86 <23229130@qq.com>
2025-11-28 11:48:34 +0800

    v1.496

--- app/page.tsx
+++ app/page.tsx
@@ -103,7 +103,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50">
       {/* 导航 */}
-      <Navigation activeTab={activeTab} onTabChange={setActiveTab} />
+      <Navigation />
       
       {/* 主要内容区域 */}
       <ResponsiveLayout className="py-6">
