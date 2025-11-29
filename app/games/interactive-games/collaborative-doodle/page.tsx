d69a4626f6ff086be535102248a822065219b1ac
FeiFan86 <23229130@qq.com>
2025-11-29 14:48:41 +0800

    v1.9.9

--- app/games/interactive-games/collaborative-doodle/page.tsx
+++ app/games/interactive-games/collaborative-doodle/page.tsx
@@ -153,7 +153,7 @@ export default function CollaborativeDoodlePage() {
   }, [timerActive, sessionTime])
 
   // 格式化时间
-  const formatTime = (seconds) => {
+  const formatTime = (seconds: number) => {
     const mins = Math.floor(seconds / 60)
     const secs = seconds % 60
     return `${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`
