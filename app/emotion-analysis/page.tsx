06d260fa40a63f79d10bf9924ef7a48a87b30de8
FeiFan86 <23229130@qq.com>
2025-11-29 13:06:41 +0800

    v1.9.4

--- app/emotion-analysis/page.tsx
+++ app/emotion-analysis/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { Navigation } from '@/components/navigation'
 import { Footer } from '@/components/footer'
 import { Button } from '@/components/ui/button'
