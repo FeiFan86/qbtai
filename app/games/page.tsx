95872e32e732a46c31ad4f7d26388c44ebd88999
FeiFan86 <23229130@qq.com>
2025-12-03 11:56:31 +0800

    v2.2.6

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -368,7 +368,7 @@ export default function GamesPage() {
                     className={`transition-all duration-300 ${isLoaded ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'}`}
                     style={{ transitionDelay: `${index * 100}ms` }}
                   >
-                    <GameCard game={game} />
+                    <GameCard game={{...game, difficulty: game.difficulty as '简单' | '中等' | '困难'}} />
                   </div>
                 ))}
               </div>
@@ -380,7 +380,7 @@ export default function GamesPage() {
                     className={`transition-all duration-300 ${isLoaded ? 'translate-x-0 opacity-100' : 'translate-x-10 opacity-0'}`}
                     style={{ transitionDelay: `${index * 50}ms` }}
                   >
-                    <GameCard game={game} size="compact" />
+                    <GameCard game={{...game, difficulty: game.difficulty as '简单' | '中等' | '困难'}} size="compact" />
                   </div>
                 ))}
               </div>
